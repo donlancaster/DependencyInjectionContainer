f071d5794ff2b035c9f527f7abde1822104eb3c3
thinkpad <donlancaster228@yandex.ru>
2021-12-07 13:34:46 +0300

    added DependenciesConfiguration, ImplementationInfo classes

---
+++ DependencyInjectionContainer/DependenciesConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer
+{
+
+
+    public enum LifeTime
+    {
+        Singleton, InstancePerDependency
+    }
+
+
+    class DependenciesConfiguration
+    {
+
+
+
+        public readonly Dictionary<Type, List<ImplementationInfo>> RegisteredDependencies;
+
+        public DependenciesConfiguration()
+        {
+            RegisteredDependencies = new Dictionary<Type, List<ImplementationInfo>>();
+        }
+
+    }
+}
